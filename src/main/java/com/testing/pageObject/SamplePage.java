165d6f0e8e67b1676670ade7a60a3e0139fe5a8d
Achiever-Anish <mohammedeesa9841@gmail.com>
2024-08-11 18:48:02 +0530

    PracticeTest
    Changes Done. RUnning Sucessfully

--- src/main/java/com/testing/pageObject/SamplePage.java
+++ src/main/java/com/testing/pageObject/SamplePage.java
@@ -4,24 +4,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class SamplePage {
 
     WebDriver driver;
+
     @FindBy(name="q")
-    WebElement search;
+   public WebElement search;
+
+    @FindBy(id="username")
+    public  WebElement emailTextBox;
+
+    @FindBy(id="password")
+   public WebElement pwdTextBox;
+
+    @FindBy(id="submit")
+    public WebElement submitButton;
+
+    @FindBy(id="error")
+   public WebElement errorMessage;
+
+    @FindBy(xpath="//*[@id='loop-container']/div/article/div[2]/p[1]/strong")
+    public WebElement successMessage;
+
+    @FindBy(xpath="//*[@id='loop-container']/div/article/div[2]/div/div/div/a")
+    public WebElement logoutButton;
+
 
     public SamplePage(WebDriver driver)
     {
         this.driver=driver;
         PageFactory.initElements(driver,this);
-    }
-
-
-    public void searchFor(String query)
-    {
-        search.sendKeys(query);
-        search.submit();;
     }
 
 
--- src/test/java/com/testing/base/BaseClass.java
+++ src/test/java/com/testing/base/BaseClass.java
@@ -1,25 +1,40 @@
 package com.testing.base;
 
+import com.testing.constants.Constant;
 import com.testing.utils.DriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
 
     protected WebDriver driver;
 
 
-    @BeforeClass
+    @BeforeMethod
     public void setup()
     {
+
         driver = DriverManager.getDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get(Constant.practice_URL);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
-    @AfterClass
+    @AfterMethod
     public void tearDown()
     {
         if(driver!=null) {
+            
             driver.quit();
+
         }
     }
 
